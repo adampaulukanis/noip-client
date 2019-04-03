ef939065a1f373228927c55dfe16f63aa299c4f6
Adam Paulukanis <adam@Adams-MacBook-Pro.local>
2019-04-03 15:12:22 +0100

    TODO: add get request to noip

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # noip-client
 
 I need something to update my account at noip with the correct external IP.
+
+## How to obtain your external IP
+
+There are a few methods of doing it
+
+- [NPM public-ip](https://github.com/sindresorhus/public-ip/blob/master/index.js)
+- [https://ipecho.net/plain](https://ipecho.net/plain)
+- Shell command **dig +short myip.opendns.com @resolver1.opendns.com**
+- My router knows my external IP so I could parse it **or** google / what is my ip / ...
+- [StackOverflow](https://stackoverflow.com/questions/1145899/how-do-i-find-out-what-my-external-ip-address-is)
+
+## Instalation
+
+Git clone the repo, npm install, cp config.json.example config.json and edit this file.
--- index.js
+++ index.js
@@ -1,5 +1,45 @@
+#!/usr/local/bin/node
+
 'use strict'
 
 const config = require('./config')
+const http = require('http')
+const fs = require('fs')
 
-console.log(config.username)
+const isItIPv4RegExp = /\d.\d.\d.\d/
+
+http.get(config.url, (res) => {
+  const { statusCode } = res
+
+  let error
+  if (statusCode !== 200) {
+    error = new Error(`Request Failed. Status Code: ${statusCode}`)
+    console.error(error.message)
+
+    // Consume response data to free up memory
+    res.resume()
+  } else {
+    res.setEncoding('utf8')
+    let rawData = ''
+    res.on('data', (chunk) => { rawData += chunk })
+    res.on('end', () => {
+      console.log(`Your external IP is ${rawData}`)
+      if (isItIPv4RegExp.test(rawData)) {
+        // Test if current and last IP's are the same
+        if (rawData !== config.lastKnownIP) {
+          // Update noip
+          console.log('updating noip...')
+          // Write to the file
+          fs.writeFileSync('./config.json', JSON.stringify(config))
+        } else {
+          // you have to do nothing
+        }
+      } else {
+        error = new Error(`Request OK but received ${rawData} which is not a proper IPv4`)
+        console.error(error)
+      }
+    })
+  }
+}).on('error', (e) => {
+  console.log(`Got error: ${e.message}`)
+})
