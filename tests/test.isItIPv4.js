a82169ea47fbe39ed37354df0e8cde6c1d40d8e8
Adam Paulukanis <adam.paulukanis@gmail.com>
2021-08-11 20:10:37 +0200

    Fix isItIPv4
    Fix #12
    
    - add more tests.
    - fix the typo in lib/isItIPv4.js: the 4th octet was NOT checked!
    - update package-lock.json: npm complained the packages used in my code
      where too old.

--- tests/test.isItIPv4.js
+++ tests/test.isItIPv4.js
@@ -9,9 +9,19 @@ const arrayOfIPs = [
   { 'ip': '1.2.3.4', 'valid': true },
   { 'ip': '11.22.33.44', 'valid': true },
   { 'ip': '192.168.1.123', 'valid': true },
-  { 'ip': '1.002.342.22', 'valid': false }
+  { 'ip': '1.002.342.22', 'valid': false },
+  { 'ip': '-1.002.342.22', 'valid': false },
+  { 'ip': '1111.2.42.22', 'valid': false },
+  { 'ip': '1111.002.42.22', 'valid': false },
+  { 'ip': '111.2222.42.22', 'valid': false },
+  { 'ip': '111.22.342.22', 'valid': false },
+  { 'ip': '111.222.42.2232', 'valid': false },
+  { 'ip': '111.222.42.222', 'valid': true },
+  { 'ip': '0.0.0.0', 'valid': true },
+  { 'ip': '000.0.0.0', 'valid': true },
+  { 'ip': '111.22.342.22.22', 'valid': false }
 ]
 
 arrayOfIPs.forEach((entry) => {
-  assert.deepEqual(isItIPv4(entry.ip), entry.valid, 'Tested IP is not what it should be')
+  assert.deepEqual(isItIPv4(entry.ip), entry.valid, `Tested IP (${entry.ip}) is valid? ${entry.valid}`)
 })
